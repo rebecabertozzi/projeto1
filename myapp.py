c32c158d21776182f34af952cb34bc7b580c8a7e
rebecabertozzi <rebecabertozzi2007@gmail.com>
2026-04-30 18:51:13 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -12,34 +12,34 @@ st.write("Escolha um gênero e descubra um filme!")
 
 filmes = {
     "Ação": [
-        {"nome": "Gladiador", "duracao": "2h 35min", "imagem": "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg"},
-        {"nome": "Batman: O Cavaleiro das Trevas", "duracao": "2h 32min", "imagem": "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg"},
-        {"nome": "Pantera Negra", "duracao": "2h 14min", "imagem": "https://upload.wikimedia.org/wikipedia/en/d/d6/Black_Panther_%28film%29_poster.jpg"},
-        {"nome": "Vingadores: Ultimato", "duracao": "3h 1min", "imagem": "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg"}
+        {"nome": "Gladiador", "duracao": "2h 35min", "imagem": "https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg"},
+        {"nome": "Batman: O Cavaleiro das Trevas", "duracao": "2h 32min", "imagem": "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"},
+        {"nome": "Pantera Negra", "duracao": "2h 14min", "imagem": "https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg"},
+        {"nome": "Vingadores: Ultimato", "duracao": "3h 1min", "imagem": "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"}
     ],
     "Comédia": [
-        {"nome": "Se Beber, Não Case", "duracao": "1h 40min", "imagem": "https://upload.wikimedia.org/wikipedia/en/b/b9/Hangoverposter09.jpg"},
-        {"nome": "Todo Mundo em Pânico", "duracao": "1h 28min", "imagem": "https://upload.wikimedia.org/wikipedia/en/6/6c/Scary_movie.jpg"},
-        {"nome": "Meninas Malvadas", "duracao": "1h 37min", "imagem": "https://upload.wikimedia.org/wikipedia/en/a/ac/Mean_Girls_film_poster.png"},
-        {"nome": "Superbad", "duracao": "1h 53min", "imagem": "https://upload.wikimedia.org/wikipedia/en/8/8b/Superbad_Poster.png"}
+        {"nome": "Se Beber, Não Case", "duracao": "1h 40min", "imagem": "https://image.tmdb.org/t/p/w500/uluhlXubGu1VxU63X9VHCLWDAYP.jpg"},
+        {"nome": "Gente Grande", "duracao": "1h 42min", "imagem": "https://image.tmdb.org/t/p/w500/tmQKjHcZcR3pPpQ3h3zX5i7z3vV.jpg"},
+        {"nome": "Todo Mundo em Pânico", "duracao": "1h 28min", "imagem": "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"},
+        {"nome": "Esposa de Mentirinha", "duracao": "1h 56min", "imagem": "https://image.tmdb.org/t/p/w500/e4fZ1sRkQn8g4w5zW0fvkYlCYwY.jpg"}
     ],
     "Romance": [
-        {"nome": "Titanic", "duracao": "3h 14min", "imagem": "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg"},
-        {"nome": "Diário de uma Paixão", "duracao": "2h 3min", "imagem": "https://upload.wikimedia.org/wikipedia/en/8/86/Posternotebook.jpg"},
-        {"nome": "Simplesmente Acontece", "duracao": "1h 42min", "imagem": "https://upload.wikimedia.org/wikipedia/en/1/1c/Love%2C_Rosie_poster.jpg"},
-        {"nome": "La La Land", "duracao": "2h 8min", "imagem": "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_%28film%29.png"}
+        {"nome": "Titanic", "duracao": "3h 14min", "imagem": "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"},
+        {"nome": "Diário de uma Paixão", "duracao": "2h 3min", "imagem": "https://image.tmdb.org/t/p/w500/qom1SZSENdmHFNZBXbtJAU0WTlC.jpg"},
+        {"nome": "A Culpa é das Estrelas", "duracao": "2h 6min", "imagem": "https://image.tmdb.org/t/p/w500/iB6GM6I2GJH0Zx8F6K5j3YF5d4E.jpg"},
+        {"nome": "La La Land", "duracao": "2h 8min", "imagem": "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg"}
     ],
     "Terror": [
-        {"nome": "Invocação do Mal", "duracao": "1h 52min", "imagem": "https://upload.wikimedia.org/wikipedia/en/1/1f/Conjuring_poster.jpg"},
-        {"nome": "Hereditário", "duracao": "2h 7min", "imagem": "https://upload.wikimedia.org/wikipedia/en/d/d9/Hereditary.png"},
-        {"nome": "O Exorcista", "duracao": "2h 2min", "imagem": "https://upload.wikimedia.org/wikipedia/en/7/7b/Exorcist_ver2.jpg"},
-        {"nome": "It: A Coisa", "duracao": "2h 15min", "imagem": "https://upload.wikimedia.org/wikipedia/en/5/5a/It_%282017%29_poster.jpg"}
+        {"nome": "Invocação do Mal", "duracao": "1h 52min", "imagem": "https://image.tmdb.org/t/p/w500/wVYREutTvI2tmxr6ujrHT704wGF.jpg"},
+        {"nome": "Hereditário", "duracao": "2h 7min", "imagem": "https://image.tmdb.org/t/p/w500/lHV8HHlhwNup2VbpiACtlKzaGIQ.jpg"},
+        {"nome": "O Exorcista", "duracao": "2h 2min", "imagem": "https://image.tmdb.org/t/p/w500/4ucLGcXVVSVnsfkGtbLY4XAius8.jpg"},
+        {"nome": "Corra!", "duracao": "1h 44min", "imagem": "https://image.tmdb.org/t/p/w500/tFXcEccSQMf3lfhfXKSU9iRBpa3.jpg"}
     ],
     "Animação": [
-        {"nome": "Shrek", "duracao": "1h 30min", "imagem": "https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28film%29_poster.jpg"},
-        {"nome": "Toy Story", "duracao": "1h 21min", "imagem": "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"},
-        {"nome": "Frozen", "duracao": "1h 42min", "imagem": "https://upload.wikimedia.org/wikipedia/en/0/05/Frozen_%282013_film%29_poster.jpg"},
-        {"nome": "Divertida Mente", "duracao": "1h 35min", "imagem": "https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_%282015_film%29_poster.jpg"}
+        {"nome": "Shrek", "duracao": "1h 30min", "imagem": "https://image.tmdb.org/t/p/w500/iB64vpL3dIObOtMZgX3RqdVdQDc.jpg"},
+        {"nome": "Toy Story", "duracao": "1h 21min", "imagem": "https://image.tmdb.org/t/p/w500/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg"},
+        {"nome": "Frozen", "duracao": "1h 42min", "imagem": "https://image.tmdb.org/t/p/w500/kgwjIb2JDHRhNk13lmSxiClFjVk.jpg"},
+        {"nome": "Divertida Mente", "duracao": "1h 35min", "imagem": "https://image.tmdb.org/t/p/w500/2H1TmgdfNtsKlU9jKdeNyYL5y8T.jpg"}
     ]
 }
 
