01f08c2dadaef6c01e8501659f2ae7d477ac671a
rebecabertozzi <rebecabertozzi2007@gmail.com>
2026-05-05 15:18:48 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -17,7 +17,7 @@ else:
   df_filtrado = df
 
 if uf:
-  f_filtrado = df_filtrado[df_filtrado['uf']==uf.upper()]
+  df_filtrado = df_filtrado[df_filtrado['uf']==uf.upper()]
 
 st.dataframe(df_filtrado)
 
