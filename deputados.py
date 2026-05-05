b0b01a81c6ee31e35c829df7b52ac78379049a82
rebecabertozzi <rebecabertozzi2007@gmail.com>
2026-05-05 14:59:36 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('deputados_2022.csv')
-st.dataframe(df)
+
+partidos = df["partido"].unique()
+
+partido_selecionado = st.selectbox("Escolha um partido", partidos)
+
+deputados_filtrados = df[df["partido"] == partido_selecionado]
+
+st.dataframe(deputados_filtrados)
+
