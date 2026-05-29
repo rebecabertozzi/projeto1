e1148086dd4cfaab9ecae7e98671f57e87ad62bc
rebecabertozzi <rebecabertozzi2007@gmail.com>
2026-05-28 21:57:50 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-df = pd.read_csv("melhores_marcas_maquiagem.csv", index_col="Marca")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(BASE_DIR, "melhores_marcas_maquiagem.csv")
+
+df = pd.read_csv(csv_path, index_col="Marca")
 
 st.title("💄 Melhores Marcas de Maquiagem")
 
